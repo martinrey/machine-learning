8f2268a22803bd7433d08c3ba313418c4133cc73
Martin Rey <marto.rey2006@gmail.com>
2016-08-31 04:03:03 -0300

    Mas atributos

--- tp1/main.py
+++ tp1/main.py
@@ -7,7 +7,7 @@ from loader_de_mensajes_para_spam_filter import LoaderDeMensajesParaSpamFilter
 from spam_filter import SpamFilter
 
 if __name__ == '__main__':
-    loader_de_mensajes_para_spam_filter = LoaderDeMensajesParaSpamFilter('datos/ham_txt.json', 'datos/spam_txt.json')
+    loader_de_mensajes_para_spam_filter = LoaderDeMensajesParaSpamFilter('datos/ham_dev.json', 'datos/spam_dev.json')
     dataframe = loader_de_mensajes_para_spam_filter.crear_dataframe()
 
     clasificador = DecisionTreeClassifier()
@@ -16,7 +16,31 @@ if __name__ == '__main__':
         CantidadDeAparicionesDePalabra('Vicodin'), CantidadDeAparicionesDePalabra('Viagra'),
         CantidadDeAparicionesDePalabra('html'), CantidadDeAparicionesDePalabra('http'),
         CantidadDeAparicionesDePalabra('.'), CantidadDeAparicionesDePalabra('\\'),
-        CantidadDeAparicionesDePalabra(' '),
+        CantidadDeAparicionesDePalabra(' '), CantidadDeAparicionesDePalabra('make'),
+        CantidadDeAparicionesDePalabra('address'), CantidadDeAparicionesDePalabra('all'),
+        CantidadDeAparicionesDePalabra('3d'), CantidadDeAparicionesDePalabra('our'),
+        CantidadDeAparicionesDePalabra('over'), CantidadDeAparicionesDePalabra('remove'),
+        CantidadDeAparicionesDePalabra('internet'), CantidadDeAparicionesDePalabra('order'),
+        CantidadDeAparicionesDePalabra('mail'), CantidadDeAparicionesDePalabra('receive'),
+        CantidadDeAparicionesDePalabra('will'), CantidadDeAparicionesDePalabra('people'),
+        CantidadDeAparicionesDePalabra('report'), CantidadDeAparicionesDePalabra('addresses'),
+        CantidadDeAparicionesDePalabra('free'), CantidadDeAparicionesDePalabra('business'),
+        CantidadDeAparicionesDePalabra('email'), CantidadDeAparicionesDePalabra('you'),
+        CantidadDeAparicionesDePalabra('credit'), CantidadDeAparicionesDePalabra('your'),
+        CantidadDeAparicionesDePalabra('font'), CantidadDeAparicionesDePalabra('money'),
+        CantidadDeAparicionesDePalabra('hp'), CantidadDeAparicionesDePalabra('hpl'),
+        CantidadDeAparicionesDePalabra('george'), CantidadDeAparicionesDePalabra('lab'),
+        CantidadDeAparicionesDePalabra('labs'), CantidadDeAparicionesDePalabra('telnet'),
+        CantidadDeAparicionesDePalabra('data'), CantidadDeAparicionesDePalabra('technology'),
+        CantidadDeAparicionesDePalabra('parts'), CantidadDeAparicionesDePalabra('pm'),
+        CantidadDeAparicionesDePalabra('direct'), CantidadDeAparicionesDePalabra('cs'),
+        CantidadDeAparicionesDePalabra('meeting'), CantidadDeAparicionesDePalabra('original'),
+        CantidadDeAparicionesDePalabra('project'), CantidadDeAparicionesDePalabra('re'),
+        CantidadDeAparicionesDePalabra('edu'), CantidadDeAparicionesDePalabra('table'),
+        CantidadDeAparicionesDePalabra('conference'), CantidadDeAparicionesDePalabra(';'),
+        CantidadDeAparicionesDePalabra('('), CantidadDeAparicionesDePalabra('['),
+        CantidadDeAparicionesDePalabra('!'), CantidadDeAparicionesDePalabra('$'),
+        CantidadDeAparicionesDePalabra('#'),
     ]
 
     spam_filter = SpamFilter(dataframe, clasificador, lista_de_atributos_a_buscar, utilizar_cache=True)
