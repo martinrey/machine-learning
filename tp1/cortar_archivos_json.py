dd0b6f066f10652c7763862f7f0571e2f5b5f964
Tobias Valdes Castro <tvaldes@eryxsoluciones.com.ar>
2016-09-22 23:16:24 -0300

    script para cortar archivos json

---
+++ tp1/cortar_archivos_json.py
@@ -0,0 +1,27 @@
+import json
+import math
+
+PORCENTAJE_DE_TEST = 0.1
+
+
+def crear_archivos_de_entrenamiento_y_test_desde(nombre_archivo):
+    archivo = open('datos/%s.json' % nombre_archivo)
+    datos_txt = json.load(archivo)
+    archivo.close()
+
+    mensajes_totales = len(datos_txt)
+    indice_de_corte = math.floor(mensajes_totales * (1 - PORCENTAJE_DE_TEST))
+    parte_de_entrenamiento = datos_txt[:indice_de_corte]
+    parte_de_test = datos_txt[indice_de_corte:]
+
+    file_entrenamiento = open('datos/%s_entrenamiento.json' % nombre_archivo, 'w')
+    json.dump(parte_de_entrenamiento, file_entrenamiento)
+    file_entrenamiento.close()
+
+    file_test = open('datos/%s_test.json' % nombre_archivo, 'w')
+    json.dump(parte_de_test, file_test)
+    file_test.close()
+
+
+crear_archivos_de_entrenamiento_y_test_desde('ham_dev')
+crear_archivos_de_entrenamiento_y_test_desde('spam_dev')
